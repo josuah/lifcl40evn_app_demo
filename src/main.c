64d02b5a95c38626885fb30b7beb88ee994d3e53
Josuah Demangeon <me@josuah.net>
2023-08-28 14:14:34 +0200

    bringup: small syntax adjustments and gitignore

--- src/main.c
+++ src/main.c
@@ -1,15 +1,12 @@
 #include <zephyr/kernel.h>
-
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
-    while (1) {
-
-		printk("Zephyr Example Application %s\n", "lifcl40evn_app_demo");
+	while (1) {
+		printk("Zephyr Example Application\n");
 		k_sleep(K_MSEC(1000));
 	}
-
 	return 0;
 }
