495f002a0c18dffeaa1d2f77ee97c05cb9ba595d
Josuah Demangeon <me@josuah.net>
2023-08-28 14:10:28 +0200

    bringup: working hello world

--- src/main.c
+++ src/main.c
@@ -1,18 +1,13 @@
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
-#include <app_version.h>
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 int main(void)
 {
-	int ret;
-	const struct device *sensor;
-
     while (1) {
 
-		printk("Zephyr Example Application %s\n", APP_VERSION_STRING);
+		printk("Zephyr Example Application %s\n", "lifcl40evn_app_demo");
 		k_sleep(K_MSEC(1000));
 	}
 
