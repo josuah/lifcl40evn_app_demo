d0aad904c508b294dce3aca38d94b933192f1759
Josuah Demangeon <me@josuah.net>
2023-08-29 02:30:26 +0200

    bringup: read a full chunk of configuration from I2C

--- src/main.c
+++ src/main.c
@@ -1,27 +1,48 @@
 #include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/shell/shell.h>
+#include <zephyr/drivers/i2c.h>
 
 LOG_MODULE_REGISTER(app);
 
+static int cmd_si5351a(const struct shell *sh, size_t argc, char **argv)
+{
+	const struct i2c_dt_spec spec = I2C_DT_SPEC_GET(DT_NODELABEL(si5351a));
+	uint8_t buf[32];
+	int ret;
+
+	ARG_UNUSED(argc);
+	ARG_UNUSED(argv);
+
+	ret = i2c_burst_read_dt(&spec, 0x00, buf, sizeof(buf));
+	if (ret < 0) {
+		shell_error(sh, "Unable to read Si5351A registers over I2C");
+		return -EIO;
+	}
+
+	shell_print(sh, "i2c_values starting from 0x0:");
+	shell_hexdump(sh, buf, sizeof(buf));
+
+	return 0;
+}
+SHELL_CMD_ARG_REGISTER(si5351a, NULL, "Si5351A debug toolbox", cmd_si5351a, 1, 0);
+
 int main(void)
 {
-        if (IS_ENABLED(CONFIG_SHELL_START_OBSCURED)) {
-                login_init();
-        }
+	const struct device *dev;
+	uint32_t dtr = 0;
 
-#if DT_NODE_HAS_COMPAT(DT_CHOSEN(zephyr_shell_uart), zephyr_cdc_acm_uart)
-        const struct device *dev;
-        uint32_t dtr = 0;
+	dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
+	if (!device_is_ready(dev)) {
+		return 0;
+	}
 
-        dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_shell_uart));
-        if (!device_is_ready(dev) || usb_enable(NULL)) {
-                return 0;
-        }
-
-        while (!dtr) {
-                uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-                k_sleep(K_MSEC(100));
-        }
-#endif
-        return 0;
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+		k_sleep(K_MSEC(100));
+	}
+	return 0;
 }
